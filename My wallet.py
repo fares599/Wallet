f6c4fcd318e01323866860015ff21c56ec14f134
Fares <faresmohamed1298@gmail.com>
2023-05-27 17:36:23 +0300

    I Create the Python file with my needs

---
+++ My wallet.py
@@ -0,0 +1,16 @@
+# My wallet design
+
+# My cash or income in the month EX : 1000 LE
+
+# distribute my money 
+# 1- principles 55 %
+# 2- charity     5 %
+# 3- Learning   10 %
+# 4- Investment 10 %
+# 5- Balance    10 %
+# 6- Welfare    10 %
+
+# ZaKah 
+# From Balance The money that 1-reach the gold 85g "24" and have 365 day 
+# I need to see my report about my money every day and every week and every month and every year see my progress graphs
+
